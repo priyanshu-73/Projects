657d191f5f3ecbbcc2d89eed9d27fc07a0316104
Priyanshu Saxena <112119073+priyanshu-73@users.noreply.github.com>
2024-05-22 22:21:38 +0530

    Create userRoutes.js

---
+++ Contact-Management-App(Express)/routes/userRoutes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const { registerUser, loginUser, currentUser } = require('../controller/users');
+const validateToken = require('../middleware/validateTokenHandler');
+const router = express.Router();
+
+router.route("/register").post(registerUser);
+
+router.route("/login").post(loginUser)
+
+router.route("/current").get(validateToken, currentUser)
+
+module.exports = router;
