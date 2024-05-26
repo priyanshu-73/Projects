800ea3a352d07fdc5bb6d4539e10ced3585f8d01
Priyanshu Saxena <112119073+priyanshu-73@users.noreply.github.com>
2024-05-26 17:02:10 +0530

    Delete Note-Making-App(NodeJS)/routes directory

--- Note-Making-App(NodeJS)/routes/index.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const { homepage, about } = require('../controllers/main');
-const router = express.Router();
-
-router.route('/').get(homepage);
-router.route('/about').get(about);
-
-
-module.exports = router;
